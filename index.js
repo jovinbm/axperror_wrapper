864eff545ca9c282d1a522cb7c5fee138afb4722
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-06-20 13:05:52 +0300

    changed html_error_friendly to render_error_friendly

--- index.js
+++ index.js
@@ -5,12 +5,12 @@
 var Axperror_wrapper = function (config) {
   var self = this;
 
-  if (typeof config.html_error_friendly !== 'function') {
-    throw new Error('config.html_error_friendly should be function');
+  if (typeof config.render_error_friendly !== 'function') {
+    throw new Error('config.render_error_friendly should be function');
   }
 
-  self.name                = 'Axperror_wrapper';
-  self.html_error_friendly = config.html_error_friendly;
+  self.name                  = 'Axperror_wrapper';
+  self.render_error_friendly = config.render_error_friendly;
 };
 
 require('./lib/index')(Axperror_wrapper);
