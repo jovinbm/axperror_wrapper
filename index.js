bc034292f716cc0dcb3b5f1be3cc823852849af0
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-06-20 12:52:35 +0300

    added Axperror_wrapper constructor to exports

--- index.js
+++ index.js
@@ -3,17 +3,16 @@
  * @constructor
  */
 var Axperror_wrapper = function (config) {
-  this.name                = 'Axperror_wrapper';
-  this.html_error_friendly = config.html_error_friendly;
-};
-
-require('./lib/index')(Axperror_wrapper);
-
-exports.Axperror_wrapper = function (config) {
+  var self = this;
 
   if (typeof config.html_error_friendly !== 'function') {
     throw new Error('config.html_error_friendly should be function');
   }
 
-  return new Axperror_wrapper(config);
+  self.name                = 'Axperror_wrapper';
+  self.html_error_friendly = config.html_error_friendly;
 };
+
+require('./lib/index')(Axperror_wrapper);
+
+exports.Axperror_wrapper = Axperror_wrapper;
